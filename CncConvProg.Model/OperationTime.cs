94ab8c05386d59c2b44f23eb02ce462eb2a2e812
tenac <tenac@localhost>
2012-01-30 23:49:10 +0100

    31

---
+++ CncConvProg.Model/OperationTime.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CncConvProg.Model
+{
+    [Serializable]
+    public class OperazioneTime
+    {
+        public TimeSpan TempoTotale
+        {
+            get { return TempoLavoro + TempoRapido; }
+        }
+
+        public double CostoConsumoUtensile
+        {
+            get
+            {
+                return TempoLavoro.TotalMinutes * ConsumoUtensilePerMinuto;
+            }
+        }
+
+
+        public TimeSpan TempoRapido { get; set; }
+
+        public TimeSpan TempoLavoro { get; set; }
+
+        public double DistanzaPercorsaLavoro { get; set; }
+
+        public double DistanzaPercorsaRapido { get; set; }
+
+        public int NumeroUtensile { get; set; }
+
+        public double ConsumoUtensilePerMinuto { get; set; }
+
+        public bool ForzaCambioUtensile { get; set; }
+
+        public string DescrizioneUtensile { get; set; }
+    }
+}
